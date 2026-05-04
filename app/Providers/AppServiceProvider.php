dc941e2c855d1df814088af1bd8ca5006301e7a9
KimAldrich <andradakimaldrich@gmail.com>
2026-05-04 22:39:19 +0800

    upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         View::composer('layouts.app', function ($view) {
             $summary = [
                 'enabled' => false,
